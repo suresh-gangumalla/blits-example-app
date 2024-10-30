0a4ddbbdd2b4bca6f81500146d06c48d2a6d6c9d
Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>
2024-10-30 17:29:18 +0530

    Isolated common code to component
    Updated colors of tiles and isolated common code
    to component for re-usability
    
    Signed-off-by: Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>

--- src/pages/FireBolt.js
+++ src/pages/FireBolt.js
@@ -22,25 +22,30 @@ import fireBoltModules from '../api/fireBolt'
 
 const Module = Blits.Component('Module', {
   template: `
-    <Element w="$w" h="$h" :color="$bColor">
-      <Text x="$w/2" y="$h/2" mount="{x:0.5, y:0.5}" :content="$name" color="$fColor" font="raleway" />
+    <Element w="$w" h="$h" :color="{left:$bgLeft , right:$bgRight}">
+      <Text x="$w/2" y="$h/2" mount="{x:0.5, y:0.5}" :content="$name" :color.transition="$fColor" font="raleway" />
     </Element>
     `,
   state() {
     return {
       w: 300,
       h: 250,
-      bColor: '#0000ffff',
-      fColor: '#000',
+      bgLeft: '#b43fcb',
+      bgRight: '#6150cb',
+      fColor: '#fff',
     }
   },
   props: ['name'],
   hooks: {
     focus() {
-      this.bColor = '#ffff00ff'
+      this.bgLeft = '#efc18f99'
+      this.bgRight = '#ffddcc'
+      this.fColor = '#000'
     },
     unfocus() {
-      this.bColor = '#0000ffff'
+      this.bgLeft = '#b43fcb'
+      this.bgRight = '#6150cb'
+      this.fColor = '#fff'
     },
   },
   input: {
@@ -53,32 +58,37 @@ const Module = Blits.Component('Module', {
 const Method = Blits.Component('Methods', {
   template: `
     <Element>
-      <Element w="$w" h="$h" :color="$nameBgColor">
-        <Text :content="$name" x="$w/2" y="$h/2" mount="0.5" :color="$fColor" font="lato" size="50" />
+      <Element w="$w" h="$h" :color="{left: $method.bgLeft, right: $method.bgRight}">
+        <Text :content="$name" x="$w/2" y="$h/2" mount="0.5" :color.transition="$method.color" font="lato" size="50" />
       </Element>
       <Element w="$w" h="50" color="$aboutBgColor" y="75">
-        <Text :content="$about" wordwrap="$w" align="center" font="lato" maxlines="1" size="28" y="6" />
+        <Text :content="$about" wordwrap="$w" align="center" font="lato" maxlines="1" size="28" y="6" color="#eee" />
       </Element>
     </Element>
     `,
   state() {
     return {
-      w: 600,
+      w: 750,
       h: 75,
-      nameBgColor: '#0000aa',
-      fColor: '#ffffffee',
+      method: {
+        color: '#fff',
+        bgLeft: '#b43fcb',
+        bgRight: '#6150cb',
+      },
       aboutBgColor: '#00000060',
     }
   },
   props: ['name', 'about'],
   hooks: {
     focus() {
-      this.nameBgColor = '#ffff00ff'
-      this.fColor = '#000'
+      this.method.bgLeft = '#efc18f99'
+      this.method.bgRight = '#ffddcc'
+      this.method.color = '#000'
     },
     unfocus() {
-      this.nameBgColor = '#0000aa'
-      this.fColor = '#ffffffee'
+      this.method.bgLeft = '#b43fcb'
+      this.method.bgRight = '#6150cb'
+      this.method.color = '#fff'
     },
   },
   input: {
@@ -95,25 +105,27 @@ const List = Blits.Component('List', {
   },
   template: `
     <Element>
-      <Element :y.transition="$y">
-        <Method
-          :for="(item, index) in $methods"
-          key="$item.name"
-          name="$item.name"
-          about="$item.about"
-          :y="$index * 160"
-          :ref="$type+$index"
-        />
+      <Element w="1920" h="700" clipping="true" y="200">
+        <Element :y.transition="$y">
+          <Method
+            :for="(item, index) in $methods"
+            key="$item.name"
+            name="$item.name"
+            about="$item.about"
+            :y="$index * 160"
+            :ref="$type+$index"
+          />
+        </Element>
       </Element>
-      <Element :x.transition="$x" y="325">
+      <Element :x.transition="$x" y="540" h="250" mount="{y:0.5}">
         <Module :for="(item, index) in $modules" key="$item.id" name="$item.name" :ref="$type+$index" x="$index * 400" />
       </Element>
     </Element>
     `,
   state() {
     return {
-      x: 400,
-      y: 100,
+      x: 0,
+      y: 0,
       activeIndex: 0,
     }
   },
@@ -158,9 +170,9 @@ const List = Blits.Component('List', {
     move(dir) {
       const next = this.activeIndex + dir
       if (this.type == 'method') {
-        this.y = next > 5 ? -(next - 5) * 160 - 100 : 100
+        this.y = next > 3 ? -(next - 3) * 160 : 0
       } else if (this.type == 'module') {
-        this.x = next > 3 ? -(next - 3) * 400 - 400 : 400
+        this.x = next > 3 ? -(next - 3) * 400 : 0
       }
       this.activeIndex = next
     },
@@ -174,7 +186,7 @@ const FireBolt = Blits.Component('FireBolt', {
   template: `
     <Element>
       <Text content="FireBolt Modules" x="960" y="80" mount="{x:0.5}" size="80" font="raleway" />
-      <List data="$moduleNames" type="module" ref="modules" />
+      <List data="$moduleNames" type="module" ref="modules" x="400" />
     </Element>
     `,
   computed: {
@@ -192,14 +204,43 @@ const FireBolt = Blits.Component('FireBolt', {
   },
 })
 
-const DeviceModule = Blits.Component('DeviceModule', {
+const ModuleTemplate = Blits.Component('ModuleTemplate', {
   components: { List },
   template: `
     <Element>
-      <Text content="Device" x="960" mount="{x:0.5}" y="50" font="raleway" size="40" />
-      <Text content="$device.about" x="960" mount="{x:0.5}" y="110" font="lato" />
-      <List data="$device.methods" x="300" ref="methods" type="method" y="100" />
-      <Text x="1400" y="540" :content="$response" size="100" mount="0.5" font="lato" :show="!$isNetworkRes" />
+      <Text content="$module.name" x="960" mount="{x:0.5}" y="50" font="raleway" size="40" />
+      <Text content="$module.about" x="960" mount="{x:0.5}" y="110" font="lato" />
+      <List data="$module.methods" x="200" ref="methods" type="method" :y="$position" />
+      <Text x="1400" y="540" :content="$response" :size="$size" mount="0.5" font="lato" :show="$response !== ''" />
+    </Element>
+    `,
+  props: [
+    'module',
+    'response',
+    {
+      key: 'size',
+      default: 100,
+    },
+    {
+      key: 'position',
+      default: 0,
+    },
+  ],
+  hooks: {
+    focus() {
+      const comp = this.$select('methods')
+      comp && comp.$focus && comp.$focus()
+    },
+  },
+})
+
+const DeviceModule = Blits.Component('DeviceModule', {
+  components: {
+    ModuleTemplate,
+  },
+  template: `
+    <Element>
+      <ModuleTemplate module="$module" :response="$response" ref="moduleTemplate" />
       <Element :show="$isNetworkRes">
         <Text :content="'State: ' + $networkRes.state" size="70" font="lato" x="1400" y="500" mount="0.5" />
         <Text :content="'Type: ' + $networkRes.type" size="70" font="lato" y="100" x="1400" y="580" mount="0.5" />
@@ -208,16 +249,15 @@ const DeviceModule = Blits.Component('DeviceModule', {
     `,
   state() {
     return {
-      device: fireBoltModules.device,
+      module: { ...fireBoltModules.device, name: 'Device' },
       response: '',
-      networkRes: {},
       isNetworkRes: false,
+      networkRes: {},
     }
   },
   hooks: {
     ready() {
       this.$listen('execute', async (api) => {
-        this.$log.info(`Execute Device ${api} API`)
         this.isNetworkRes = false
         switch (api) {
           case 'make':
@@ -231,6 +271,7 @@ const DeviceModule = Blits.Component('DeviceModule', {
             break
           case 'network':
             Object.assign(this.networkRes, await Device.network())
+            this.response = ''
             this.isNetworkRes = true
             break
           case 'platform':
@@ -240,25 +281,24 @@ const DeviceModule = Blits.Component('DeviceModule', {
       })
     },
     focus() {
-      const comp = this.$select('methods')
+      const comp = this.$select('moduleTemplate')
       comp && comp.$focus && comp.$focus()
     },
   },
 })
 
 const AccountModule = Blits.Component('AccountModule', {
-  components: { List },
+  components: {
+    ModuleTemplate,
+  },
   template: `
     <Element>
-      <Text content="Account" x="960" mount="{x:0.5}" y="50" font="raleway" size="40" />
-      <Text content="$account.about" x="960" mount="{x:0.5}" y="110" font="lato" />
-      <List data="$account.methods" x="300" ref="methods" y="300" type="method" />
-      <Text x="1400" y="540" :content="$response" :size="$size" mount="0.5" font="lato" />
+      <ModuleTemplate module="$module" :response="$response" :size="$size" ref="moduleTemplate" position="200" />
     </Element>
     `,
   state() {
     return {
-      account: fireBoltModules.account,
+      module: { ...fireBoltModules.account, name: 'Account' },
       response: '',
       size: 100,
     }
@@ -280,33 +320,31 @@ const AccountModule = Blits.Component('AccountModule', {
       })
     },
     focus() {
-      const comp = this.$select('methods')
+      const comp = this.$select('moduleTemplate')
       comp && comp.$focus && comp.$focus()
     },
   },
 })
 
 const LocalizationModule = Blits.Component('LocalizationModule', {
-  components: { List },
+  components: {
+    ModuleTemplate,
+  },
   template: `
     <Element>
-      <Text content="Localization" x="960" mount="{x:0.5}" y="50" font="raleway" size="40" />
-      <Text content="$localization.about" x="960" mount="{x:0.5}" y="110" font="lato" />
-      <List data="$localization.methods" x="300" ref="methods" y="100" type="method" />
-      <Text x="1400" y="540" :content="$response" size="100" mount="0.5" font="lato" />
+      <ModuleTemplate module="$module" :response="$response" ref="moduleTemplate" />
     </Element>
     `,
   state() {
     return {
-      localization: fireBoltModules.localization,
+      module: { ...fireBoltModules.localization, name: 'Localization' },
       response: '',
     }
   },
   hooks: {
     ready() {
       this.$listen('execute', async (api) => {
-        this.$log.info(`Execute Keyboard ${api} API`)
-        this.response = 'null'
+        this.response = ''
         switch (api) {
           case 'countryCode':
             this.response = await Localization.countryCode()
@@ -327,7 +365,7 @@ const LocalizationModule = Blits.Component('LocalizationModule', {
       })
     },
     focus() {
-      const comp = this.$select('methods')
+      const comp = this.$select('moduleTemplate')
       comp && comp.$focus && comp.$focus()
     },
   },
@@ -343,7 +381,7 @@ export const FireBoltRoutes = [
     component: DeviceModule,
   },
   {
-    path: '/examples/fireboat/account',
+    path: '/examples/firebolt/account',
     component: AccountModule,
   },
   {
